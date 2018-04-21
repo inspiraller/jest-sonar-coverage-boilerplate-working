cb0730b9bc94d2db5739d23f66f0fe5aae73cd79
steveltomlin <steveltomlin@gmail.com>
2018-04-21 13:46:36 +0100

    add test to cover 60%

--- specs/example.spec.js
+++ specs/example.spec.js
@@ -1,6 +1,11 @@
-const {somethingElse} = require('../src/somefile');
+const {somethingElse, hello} = require('../src/somefile');
 describe("somethingElse", () => {
   it("should show me", () => {
       expect(somethingElse('me')).toBe('me');
+  });
+});
+describe("hello", () => {
+  it("should show hello", () => {
+      expect(hello()).toBe('hello');
   });
 });
